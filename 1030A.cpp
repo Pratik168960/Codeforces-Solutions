8e83241202eb6c333f51cbb96eb844a614d6d2ad
Pratik <pratiksingh111252@gmail.com>
2026-01-13 22:14:44 +0530

    Add Codeforces problem 1030A solution

---
+++ 1030A.cpp
@@ -0,0 +1,36 @@
+// Codeforces Problem 1030A
+// Status: Accepted
+// Language: C++
+
+
+#include<iostream>
+using namespace std ;
+ 
+int main() {
+    
+    int n ;
+    cin >> n ;
+    
+    
+    int opinion ;
+    bool isHard = false ;
+    
+    for (int i = 0 ; i < n ; i++) {
+        
+        cin >> opinion ;
+        
+        if (opinion == 1) {
+            isHard = true ;
+            break ;
+        }
+    }
+    
+    if (isHard) {
+        cout << "HARD" << endl ;
+    } else {
+        cout << "EASY" << endl ;
+    }
+    
+    return 0;
+}
+
