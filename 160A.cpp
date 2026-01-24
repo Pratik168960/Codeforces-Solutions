53d35f55ba3ba525a82d75c2de8a4d9bfd2c7de4
Pratik <pratiksingh111252@gmail.com>
2026-01-24 23:41:00 +0530

    Add Codeforces problem 160A solution

---
+++ 160A.cpp
@@ -0,0 +1,39 @@
+// Codeforces Problem 160A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> coins(n);
+    int totalSum = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+        totalSum += coins[i];
+    }
+ 
+    sort(coins.begin(), coins.end(), greater<int>());
+ 
+    int mySum = 0;
+    int count = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        mySum += coins[i];
+        count++;
+        if (mySum > totalSum - mySum) {
+            break;
+        }
+    }
+ 
+    cout << count << endl;
+ 
+    return 0;
+} 
