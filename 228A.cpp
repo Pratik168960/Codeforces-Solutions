70d434ffb4e836cef86e4906714da225a7630cf9
Pratik <pratiksingh111252@gmail.com>
2026-01-27 23:33:07 +0530

    Add Codeforces problem 228A solution

---
+++ 228A.cpp
@@ -0,0 +1,30 @@
+// Codeforces Problem 228A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+#include <algorithm>
+using namespace std ;
+ 
+int main() {
+    int s[4];
+ 
+    for (int i = 0; i < 4; i++) {
+        cin >> s[i];
+    }
+ 
+ 
+    sort(s, s + 4);
+ 
+    int shoesToBuy = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        if (s[i] == s[i + 1]) {
+            shoesToBuy++;
+        }
+    }
+ 
+    cout << shoesToBuy << endl;
+ 
+    return 0;
+}
