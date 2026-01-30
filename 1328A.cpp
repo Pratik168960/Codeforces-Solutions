59be46ad13329352eb5cb996fa61000418a910f6
Pratik <pratiksingh111252@gmail.com>
2026-01-30 23:52:17 +0530

    Add Codeforces problem 1328A solution

---
+++ 1328A.cpp
@@ -0,0 +1,22 @@
+// Codeforces Problem 1328A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int t ;
+    cin >> t ;
+    while (t--) {
+        int a, b ; 
+        cin >> a >> b ;
+        if (a % b == 0) {
+            cout << 0 << endl ;
+        } else {
+            cout << b - (a % b) << endl ;
+        }
+    }
+    return 0;
+}
