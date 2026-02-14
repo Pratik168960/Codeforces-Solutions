7bf6cdef43a1b27e77e43bd85fca0282bf47b491
Pratik <pratiksingh111252@gmail.com>
2026-02-14 23:26:02 +0530

    Add Codeforces problem 1335A solution

---
+++ 1335A.cpp
@@ -0,0 +1,22 @@
+// Codeforces Problem 1335A
+// Status: Accepted
+// Language: C++ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t ;
+    cin >>  t ;
+    
+    while (t--) {
+        
+        long long n ;
+        cin >> n ;
+        
+        cout << (n - 1) / 2 << endl ;
+    }
+    
+    return 0 ;
+}
