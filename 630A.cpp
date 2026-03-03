a1781a4c0090c85acb6d6280131f789530a6dfe5
Pratik <pratiksingh111252@gmail.com>
2026-03-03 22:14:13 +0530

    Add Codeforces problem 630A solution

---
+++ 630A.cpp
@@ -0,0 +1,16 @@
+// Codeforces Problem 630A
+// Status: Accepted
+// Language: C++
+
+#include<iostream>
+using namespace std ;
+
+int main (){
+    
+    long long n ; 
+    cin >> n ; 
+    
+    cout << 25 << endl ;
+    
+    return 0 ; 
+}
