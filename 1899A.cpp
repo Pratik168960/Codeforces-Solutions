5400ded061f3ebbd13744e200de719bc0613385d
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-06 23:42:01 +0530

    Add Codeforces problem 1899A solution

---
+++ 1899A.cpp
@@ -0,0 +1,25 @@
+// Codeforces Problem 1899A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std ;
+
+int main() {
+    
+    int t, n ;
+    cin >> t ;
+    
+    
+    while (t--) {
+        cin >> n ;
+        if (n % 3 == 0) {
+            cout << "Second"<< endl ;
+        } else {
+            cout << "First" << endl ;
+        }
+    }
+    
+    
+    return 0 ;
+}
