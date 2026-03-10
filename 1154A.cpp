48c49a473099017ad356cf47087e1d5a23eaefe5
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-10 23:01:12 +0530

    Add Codeforces problem 1154A solution

---
+++ 1154A.cpp
@@ -0,0 +1,23 @@
+// Codeforces Problem 1154A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std ;
+
+int main() {
+    vector<int> x(4) ;
+    for (int i = 0 ; i < 4 ; i++) {
+        cin >> x[i] ;
+    }
+    
+    sort(x.begin(), x.end()) ;
+    
+    cout << x[3] - x[0] << " " << x[3] - x[1] << " " << x[3] - x[2] << endl ;
+    
+    return 0 ;
+    
+    
+} 
