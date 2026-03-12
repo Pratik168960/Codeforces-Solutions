79f341e943bbdbaa6c1613c20195ed0219d58d5d
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-12 23:45:57 +0530

    Add Codeforces problem 1475A solution

---
+++ 1475A.cpp
@@ -0,0 +1,23 @@
+// Codeforces Problem 1475A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std ;
+
+int main() {
+    
+    int t ;
+    cin >> t ;
+    
+    while (t--) {
+        long long n;
+        cin >> n;
+        if (n & (n - 1)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
