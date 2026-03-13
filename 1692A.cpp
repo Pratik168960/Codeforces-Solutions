456e93e7c5bff20bc2a6f60812fa7e8d387dc72c
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-13 23:39:27 +0530

    Add Codeforces problem 1692A solution

---
+++ 1692A.cpp
@@ -0,0 +1,36 @@
+// Codeforces Problem 1692A
+// Status: Accepted
+// Language: C++ 
+
+#include<iostream>
+using namespace std ; 
+
+int main (){
+    
+    int t ; 
+    cin >> t ;
+    
+    while ( t-- ){
+        
+        int a, b, c, d ;
+        cin >> a >> b >> c >> d ;
+        int count = 0; 
+        
+        
+        if ( a < b ){
+            count++ ;
+        }
+        if ( a < c ){
+            count++ ; 
+        }
+        if ( a < d ){
+            count++ ; 
+        }
+        
+        
+        
+        cout << count << endl ; 
+    }
+    return 0 ; 
+    
+}
