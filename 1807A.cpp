c34f0df8db287390716526a727300d2394fd3bf4
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-16 23:50:59 +0530

    Add Codeforces problem 1807A solution

---
+++ 1807A.cpp
@@ -0,0 +1,26 @@
+// Codeforces Problem 1807A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std ;
+
+int main() {
+    
+    int t ;
+    cin >> t ;
+    
+    while (t--) {
+    
+        int a, b, c ;
+        cin >> a >> b >> c ;
+        
+        if (a + b == c) {
+            cout << "+\n";
+        } else {
+            cout << "-\n";
+        }
+    }
+    
+    return 0 ;
+}
