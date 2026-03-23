33610e137d115d58ceeb8ee1c0c1fd1659123e89
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-23 23:56:07 +0530

    Add Codeforces problem 179A solution

---
+++ 1791A.cpp
@@ -0,0 +1,24 @@
+// Codeforces Problem 1791A
+// Status: Accepted
+// Language: C++ 
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    string s = "codeforces";
+    while (t--) {
+        char c;
+        cin >> c;
+        if (count(s.begin(), s.end(), c)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+} 
