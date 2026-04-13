761c8672d39a1cf7eddab333c25b3ee89f8ea919
Pratik Singh <pratiksingh111252@gmail.com>
2026-04-13 23:37:31 +0530

    Add Codeforces problem 1791C solution

---
+++ 1791C.cpp
@@ -0,0 +1,26 @@
+// Codeforces Problem 1791C
+// Status: Accepted
+// Language: C++
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        int l = 0, r = n - 1;
+        while (l < r && s[l] != s[r]) {
+            l++;
+            r--;
+        }
+        cout << r - l + 1 << "\n";
+    }
+    return 0;
+}
