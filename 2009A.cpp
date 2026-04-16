d5e50e9c89f2bbe19a38d6dd58ab9a58e2ab19e8
Pratik Singh <pratiksingh111252@gmail.com>
2026-04-16 23:58:00 +0530

    Add Codeforces problem 2009A solution

---
+++ 2009A.cpp
@@ -0,0 +1,17 @@
+// Codeforces Problem 2009A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, a, b;
+    cin >> t;
+    while (t--) {
+        cin >> a >> b;
+        cout << b - a << '\n';
+    }
+    return 0;
+}
