6f7b40b2f0570b7c1cfebab7aa5da3ae4ca7946d
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-08 23:52:30 +0530

    Add LeetCode problem 1858A solution

---
+++ 1858A.cpp
@@ -0,0 +1,21 @@
+// Codeforces Problem 1858A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        if (a + c % 2 > b) {
+            cout << "First\n";
+        } else {
+            cout << "Second\n";
+        }
+    }
+    return 0;
+}
