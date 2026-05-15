3d86a10d86925373ae8f166ade58fa4197f6ae58
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-15 22:42:58 +0530

    Add Codeforces problem 137A solution

---
+++ 137A.cpp
@@ -0,0 +1,17 @@
+// Codeforces Problem 137A
+// Status: Accepted
+// Language: C++ 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << n / 2 << "\n";
+    }
+    return 0;
+}
