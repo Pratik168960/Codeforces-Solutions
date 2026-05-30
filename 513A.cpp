ec8b5b042311cf6e6bd9cb10e784ac402b571f5f
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-30 22:33:49 +0530

    Add Codeforces problem 513A solution

---
+++ 513A.cpp
@@ -0,0 +1,13 @@
+// Codeforces Problem 513A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a > b ? "First" : "Second");
+    return 0;
+}
