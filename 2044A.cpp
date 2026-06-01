ed7f1db67f3548bd8b10e288e5cb42cf8f9f55b3
Pratik Singh <pratiksingh111252@gmail.com>
2026-06-01 22:45:32 +0530

    Add Codeforces problem 2044A solution

---
+++ 2044A.cpp
@@ -0,0 +1,16 @@
+// Codeforces Problem 2044A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << n - 1 << "\n";
+    }
+}
