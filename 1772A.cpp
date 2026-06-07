a135ea6d5a88dc72b79a12481f84a0629707e019
Pratik Singh <pratiksingh111252@gmail.com>
2026-06-07 23:00:51 +0530

    Add Codeforces problem 1772A solution

---
+++ 1772A.cpp
@@ -0,0 +1,17 @@
+// Codeforces Problem 1772A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+int t, a, b;
+char c;
+
+int main() {
+    cin >> t;
+    while (t--) {
+        cin >> a >> c >> b;
+        cout << a + b << '\n';
+    }
+}
