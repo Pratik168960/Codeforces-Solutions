0b3c61d011f2b105dd775fc2c233cf160e55d844
Pratik Singh <pratiksingh111252@gmail.com>
2026-06-17 23:57:38 +0530

    Add Codeforces problem 1353B solution

---
+++ 1353B.cpp
@@ -0,0 +1,44 @@
+// Codeforces Problem 1353B
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        for (int i = 0; i < n; ++i) cin >> b[i];
+        
+        sort(a.begin(), a.end());
+        sort(b.rbegin(), b.rend());
+        
+        for (int i = 0; i < k; ++i) {
+            if (b[i] > a[i]) {
+                a[i] = b[i];
+            } else {
+                break; 
+            }
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += a[i];
+        }
+        cout << sum << "\n";
+    }
+    
+    return 0;
+}
