05727bff0af71aee6bb0272822fc924412e583db
Pratik Singh <pratiksingh111252@gmail.com>
2026-07-01 23:52:02 +0530

    Add Codeforces problem 2241A solution

---
+++ 2241A.cpp
@@ -0,0 +1,27 @@
+// Codeforces Problem 2241A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        
+        if (x % y == 0) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    
+    return 0;
+}
