ea76d7d2807756c64e0536dad2fb90312979fc59
Pratik Singh <pratiksingh111252@gmail.com>
2026-07-29 23:58:17 +0530

    Add Codeforces problem 1814Asolution

---
+++ 1814A.cpp
@@ -0,0 +1,21 @@
+// Codeforces Problem 1814A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+        if (n % 2 == 0 || k % 2 != 0) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
