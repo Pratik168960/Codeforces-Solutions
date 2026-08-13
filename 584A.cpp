a6b184308c677ee5c0a1b9d4cd45775f07f2f0f4
Pratik Singh <pratiksingh111252@gmail.com>
2026-08-13 23:46:42 +0530

    Add Codeforces problem 584A solution

---
+++ 584A.cpp
@@ -0,0 +1,30 @@
+// Codeforces Problem 584A
+// Status: Accepted
+// Language: C++
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n, t;
+    if (cin >> n >> t) {
+        if (n == 1 && t == 10) {
+            cout << -1 << "\n";
+        } else {
+            if (t == 10) {
+                cout << 1;
+            } else {
+                cout << t;
+            }
+            
+            for (int i = 0; i < n - 1; i++) {
+                cout << 0;
+            }
+            cout << "\n";
+        }
+    }
+    
+    return 0;
+}
